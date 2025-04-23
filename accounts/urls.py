985c4a6d01a5649a319b37f8e770748678e0efa9
Tushar57517 <sainitushar575@gmail.com>
2025-04-23 21:57:29 +0530

    feat(accounts): add logout functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,8 @@ from .views import (
     RefreshTokenView,
     PasswordChangeView,
     PasswordResetConfirmView,
-    PasswordResetRequestView
+    PasswordResetRequestView,
+    LogoutView
 )
 
 urlpatterns = [
@@ -17,4 +18,5 @@ urlpatterns = [
     path("change-password/", PasswordChangeView.as_view(), name="change-password"),
     path('reset-password/', PasswordResetRequestView.as_view(), name="reset-password"),
     path('reset-password-confirm/', PasswordResetConfirmView.as_view(), name="reset-password-confirm"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -106,3 +106,21 @@ class PasswordResetConfirmView(APIView):
             serializer.save()
             return Response({"message": "Password reset successful"}, status=200)
         return Response(serializer.errors, status=400)
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+
+            if not refresh_token:
+                return Response({"error":"refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response({"message":"successfully logged out"}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error":"invalid token"}, status=status.HTTP_400_BAD_REQUEST)
